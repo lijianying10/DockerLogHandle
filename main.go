23bbcf65fa7d62635621aafda36abd78308eeed6
root <lijianying12@gmail.com>
2016-03-06 11:36:35 +0800

    update file module and tcp module

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"net"
-	"os"
 )
 
 var (
@@ -29,42 +26,6 @@ func main() {
 	}
 
 	if *FileMod == true {
-		go OpenFile()
-	}
-}
-
-func OpenFile() {
-
-}
-
-func OpenServer() {
-	ln, err := net.Listen("tcp", ":"+*port)
-	if err != nil {
-		fmt.Println("err listen", err.Error())
-		os.Exit(2)
-	}
-
-	for {
-		conn, _ := ln.Accept()
-		go ConnHandler(conn)
-	}
-
-}
-
-// ConnHandler test godoc
-func ConnHandler(conn net.Conn) {
-	defer conn.Close()
-	for {
-		conn.Write([]byte("Input container what you want"))
-		message, err := bufio.NewReader(conn).ReadString('\n')
-		// handle client close connection
-		if err != nil {
-			fmt.Println(err.Error() + "Connection going to close , thread close!")
-			conn.Close()
-			return
-		}
-		fmt.Print("Message Received:", string(message))
-		conn.Write([]byte("you are watching container :" + message))
-		Dc(conn, message[:len(message)-2])
+		go FileModuleLoad()
 	}
 }
---
+++ tcpserver.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func OpenServer() {
+	ln, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		fmt.Println("err listen", err.Error())
+		os.Exit(2)
+	}
+
+	for {
+		conn, _ := ln.Accept()
+		go ConnHandler(conn)
+	}
+
+}
+
+// ConnHandler test godoc
+func ConnHandler(conn net.Conn) {
+	defer conn.Close()
+	for {
+		conn.Write([]byte("Input container what you want"))
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		// handle client close connection
+		if err != nil {
+			fmt.Println(err.Error() + "Connection going to close , thread close!")
+			conn.Close()
+			return
+		}
+		fmt.Print("Message Received:", string(message))
+		conn.Write([]byte("you are watching container :" + message))
+		Dc(conn, message[:len(message)-2])
+	}
+}
